31b8e61f55d2ebd285742a6b519ce1984d3ea3cc
cmcwil0 <111789393+cmcwil0@users.noreply.github.com>
2024-11-01 17:03:49 -0500

    Creating Posts.py

--- app.py
+++ app.py
@@ -37,4 +37,21 @@ def index():
 # route to create a post
 
 
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash("Title is required")
+        elif not content:
+            flash('Content is required')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
 app.run(port=5008)
